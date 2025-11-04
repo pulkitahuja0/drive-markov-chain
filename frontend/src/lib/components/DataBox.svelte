9543d755c46f890a3e92a46a3ee7e8b58cf306a9
pulkitahuja0 <140126488+pulkitahuja0@users.noreply.github.com>
2025-11-04 12:02:59 -0800

    show top 10 next outcomes

--- frontend/src/lib/components/DataBox.svelte
+++ frontend/src/lib/components/DataBox.svelte
@@ -2,14 +2,20 @@
 	import { keyToLabel } from '$lib/helpers';
 
 	let { label, data = $bindable() } = $props();
+
+    let top10 = $derived.by(() => {
+        let a: [string, number][] = [];
+        Object.keys(data).map(key => a.push([key, data[key]]));
+        return a.sort((x, y) => y[1] - x[1]).slice(0, 10);
+    });
 </script>
 
 <div class="self-start border-2 border-black">
 	<div class="m-3">
 		<div class="text-lg">{label}</div>
 		<ul>
-			{#each Object.keys(data).slice(0, 20) as key}
-				<li>{keyToLabel(key)}: {Math.round(data[key] * 1000) / 10}%</li>
+			{#each top10 as item}
+				<li>{keyToLabel(item[0])}: {Math.round(item[1] * 1000) / 10}%</li>
 			{/each}
 		</ul>
 	</div>
