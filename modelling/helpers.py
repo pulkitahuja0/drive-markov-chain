e8dc41636434542befcd1a55166c4121b53b29b7
pulkitahuja0 <140126488+pulkitahuja0@users.noreply.github.com>
2025-11-09 17:06:37 -0800

    most_recent_nfl_szn helper

---
+++ modelling/helpers.py
@@ -0,0 +1,13 @@
+from datetime import date
+
+def most_recent_nfl_szn():
+    today = date.today()
+
+    year = today.year
+    month = today.month
+
+    print(today.month)
+
+    if month < 3:
+        return year - 2
+    return year - 1
