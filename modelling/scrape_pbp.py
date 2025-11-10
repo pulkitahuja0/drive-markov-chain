1a0ba8982a74333194098a938f4c8c590ab70953
pulkitahuja0 <140126488+pulkitahuja0@users.noreply.github.com>
2025-11-09 17:11:59 -0800

    scrape_pbp.py will scrape 2015 to most recent szn

--- modelling/scrape_pbp.py
+++ modelling/scrape_pbp.py
@@ -3,66 +3,68 @@ import json
 import sys
 import os
 
+from helpers import most_recent_nfl_szn
 import polars as pl
 import nflreadpy as nfl
 
-year = int(sys.argv[1])
+latest_szn = most_recent_nfl_szn()
 
-pbp = nfl.load_pbp([year]).filter(~pl.col("play_type").is_in(["no_play", "kickoff", "extra_point"]))
+for year in range(2015, latest_szn + 1):
+    pbp = nfl.load_pbp([year]).filter(~pl.col("play_type").is_in(["no_play", "kickoff", "extra_point"]))
 
-data = defaultdict(lambda: defaultdict(int))
+    data = defaultdict(lambda: defaultdict(int))
 
-end_states = set(["touchdown", "made_fg", "bad_fg", "turnover", "punt"])
+    end_states = set(["touchdown", "made_fg", "bad_fg", "turnover", "punt"])
 
-for drive_df in pbp.partition_by(["game_id", "drive"], maintain_order=True):
-    if drive_df.select(pl.col("play_type").is_in(["qb_kneel"]).any()).item():
-        continue
-
-    last_play = drive_df.row(drive_df.height - 1, named=True)
-
-    # If a drive doesn't end with a turnover, score or punt, shouldn't be used in data
-    if not (last_play["play_type"] == "punt" or last_play["touchdown"] == 1 or last_play["play_type"] == "field_goal" or last_play["interception"] == 1 or last_play["fumble_lost"] == 1 or last_play["fourth_down_failed"] == 1):
-        continue
-
-    for i in range(drive_df.height):
-        play = drive_df.row(i, named=True)
-        play_id = f"{play["down"]}_{play["ydstogo"]}_{play["yardline_100"]}"
-
-        if play["extra_point_attempt"] == 1 or play["two_point_attempt"] == 1: # Skip xp or 2pt plays
+    for drive_df in pbp.partition_by(["game_id", "drive"], maintain_order=True):
+        if drive_df.select(pl.col("play_type").is_in(["qb_kneel"]).any()).item():
             continue
 
-        if (i != 0):
-            prev_play = drive_df.row(i - 1, named=True)
-            prev_play_id = f"{prev_play["down"]}_{prev_play["ydstogo"]}_{prev_play["yardline_100"]}"
-            data[prev_play_id][play_id] += 1
+        last_play = drive_df.row(drive_df.height - 1, named=True)
 
-        if play["touchdown"] == 1:
-            data[play_id]["touchdown"] += 1
-            continue
-        elif play["play_type"] == "field_goal":
-            if play["field_goal_result"] == "made":
-                data[play_id]["made_fg"] += 1
-            else:
-                data[play_id]["bad_fg"] += 1
-            continue
-        elif play["safety"] == 1:
-            data[play_id]["safety"] += 1
+        # If a drive doesn't end with a turnover, score or punt, shouldn't be used in data
+        if not (last_play["play_type"] == "punt" or last_play["touchdown"] == 1 or last_play["play_type"] == "field_goal" or last_play["interception"] == 1 or last_play["fumble_lost"] == 1 or last_play["fourth_down_failed"] == 1):
             continue
 
-        if play["fourth_down_failed"] == 1 or play["interception"] == 1 or play["fumble_lost"] == 1:
-            data[play_id]["turnover"] += 1
-            continue
-    
-        if play["play_type"] == "punt":
-            data[play_id]["punt"] += 1
-            continue
+        for i in range(drive_df.height):
+            play = drive_df.row(i, named=True)
+            play_id = f"{play["down"]}_{play["ydstogo"]}_{play["yardline_100"]}"
 
-file_name = f"output_{year}_szn.json"
-filepath = os.path.join("output", file_name)
+            if play["extra_point_attempt"] == 1 or play["two_point_attempt"] == 1: # Skip xp or 2pt plays
+                continue
 
-os.makedirs("output", exist_ok=True)
+            if (i != 0):
+                prev_play = drive_df.row(i - 1, named=True)
+                prev_play_id = f"{prev_play["down"]}_{prev_play["ydstogo"]}_{prev_play["yardline_100"]}"
+                data[prev_play_id][play_id] += 1
 
-with open(filepath, "w") as json_file:
-    json.dump(data, json_file, separators=(",", ":"))
+            if play["touchdown"] == 1:
+                data[play_id]["touchdown"] += 1
+                continue
+            elif play["play_type"] == "field_goal":
+                if play["field_goal_result"] == "made":
+                    data[play_id]["made_fg"] += 1
+                else:
+                    data[play_id]["bad_fg"] += 1
+                continue
+            elif play["safety"] == 1:
+                data[play_id]["safety"] += 1
+                continue
 
-print(f"Dumped {len(data.keys())} play by play data state transitions from the {year} season.")
+            if play["fourth_down_failed"] == 1 or play["interception"] == 1 or play["fumble_lost"] == 1:
+                data[play_id]["turnover"] += 1
+                continue
+        
+            if play["play_type"] == "punt":
+                data[play_id]["punt"] += 1
+                continue
+
+    file_name = f"output_{year}_szn.json"
+    filepath = os.path.join("output", file_name)
+
+    os.makedirs("output", exist_ok=True)
+
+    with open(filepath, "w") as json_file:
+        json.dump(data, json_file, separators=(",", ":"))
+
+    print(f"Dumped {len(data.keys())} play by play data state transitions from the {year} season.")
